f7e32b05396cd97e509de0710b01ba0f75181236
yakattak <stevecalebrese@gmail.com>
2022-03-06 12:38:08 -0500

    delete and add

--- public/lib/noteAddRemove.js
+++ public/lib/noteAddRemove.js
@@ -1,4 +1,37 @@
 const fs = require('fs');
+const { findSourceMap } = require('module');
 const path = require('path');
 
-function createNewNote() {};
+function createNewNote(body, noteArray) {
+    const newNote = body;
+    oldNotes.push(newNote);
+
+    fs.writeFileSync(
+        path.join(__dirname, '../db/db.json'),
+        JSON.stringify({noteArray}, null, 2)
+    );
+    return newNote;
+};
+
+function removeNote(noteArray, id) {
+    const deleteNote = parseInt(id);
+    noteArray.splice(deleteNote, 1);
+
+
+    for (let i = deleteNote; i< noteArray.length; i++) {
+    noteArray[i].id = i.toString();
+    };
+
+    fs.writeFileSync(
+        path.join(__dirname, '../db/db.json'),
+        JSON.stringify({
+            noteArray}, null, 2)
+        
+    )
+
+        }
+
+module.exports = {
+    createNewNote,
+    removeNote
+}
