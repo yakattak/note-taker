17745d9137c069dd5398dab498f985f888a63389
yakattak <stevecalebrese@gmail.com>
2022-03-06 12:29:42 -0500

    notesroutes

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,8 +1,29 @@
 const router = require('express').Router();
 
+const res = require('express/lib/response');
 const notes = require('../../db/db.json');
 
 const {
     createNewNote,
     removeNote
-} = require ('../../lib/notes.js');
+} = require ('../../lib/noteAddRemove.js');
+
+
+router.get('/notes', (req, res) =>{
+    const oldNotes = notes;
+    res.json(oldNotes);
+}
+);
+
+router.post('/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    const newNote = createNewNote(req.body, notes);
+    res.json(newNote);
+})
+
+router.delete('/notes/:id', (req, res) => {
+    removeNote(notes, req.params.id);
+    res.json(notes);
+})
+
+module.exports = router;
